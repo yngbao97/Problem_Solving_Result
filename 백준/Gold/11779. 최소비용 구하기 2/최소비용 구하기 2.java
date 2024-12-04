508ddb881c401b54034c110d4d99c21b67e1eba9
Tea._.pot <156047141+yngbao97@users.noreply.github.com>
2024-12-05 00:41:48 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 588 ms, Memory: 61776 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,84 +1,96 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
 public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
 
-    public static void main(String[] args) throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception {
 
-        int n = Integer.parseInt(br.readLine());    // 도시(정점)의 개수
-        int m = Integer.parseInt(br.readLine());    // 버스(간선)의 개수
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<City>[] adj = new List[n+1];
-        for (int i = 0; i <= n; i++) {
-            adj[i] = new ArrayList<>();
-        }
+		int n = Integer.parseInt(br.readLine());	// 도시(정점)의 개수
+		int m = Integer.parseInt(br.readLine());	// 버스(간선)의 개수
 
-        for (int i = 0; i < m; i++) {
-            String[] input = br.readLine().split(" ");
-            int dep = Integer.parseInt(input[0]);
-            int dest = Integer.parseInt(input[1]);
-            int cost = Integer.parseInt(input[2]);
+		@SuppressWarnings("unchecked")
+		List<City>[] adj = (List<City>[]) new List[n+1];
+		for (int i = 0; i <= n; i++) {
+			adj[i] = new ArrayList<City>();
+		}
 
-            adj[dep].add(new City(cost, dest));
-        }
+		for (int i = 0; i < m; i++) {
+			String[] input = br.readLine().split(" ");
+			int dep = Integer.parseInt(input[0]);
+			int dest = Integer.parseInt(input[1]);
+			int cost = Integer.parseInt(input[2]);
 
-        String[] input = br.readLine().split(" ");
-        int start = Integer.parseInt(input[0]);
-        int end = Integer.parseInt(input[1]);
+			adj[dep].add(new City(cost, dest));
+		}
 
-        int[] dist = new int[n+1];
+		String[] input = br.readLine().split(" ");
+		int start = Integer.parseInt(input[0]);
+		int end = Integer.parseInt(input[1]);
+
+		int[] p = new int[n+1];	// 이전 도시 저장
+		int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        int[] prev = new int[n+1];
 
-        PriorityQueue<City> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-        dist[start] = 0;
-        pq.offer(new City(0, start));
+		PriorityQueue<City> pq = new PriorityQueue<>();
+		pq.add(new City(0, start));
+		dist[start] = 0;
 
-        while (!pq.isEmpty()) {
-            City curr = pq.poll();
+		while (!pq.isEmpty()) {
 
-            if (curr.cost > dist[curr.dest]) continue;
+			City curr = pq.poll();
 
-            for (City next : adj[curr.dest]) {
-                int newCost = curr.cost + next.cost;
-                if (newCost < dist[next.dest]) {
-                    dist[next.dest] = newCost;
-                    prev[next.dest] = curr.dest;
-                    pq.offer(new City(newCost, next.dest));
-                }
-            }
-        }
+			if (curr.cost > dist[curr.dest]) continue;	// 이미 저장된 최소 거리보다 길면 패스
 
-        // Reconstruct path
-        List<Integer> path = new ArrayList<>();
-        for (int at = end; at != 0; at = prev[at]) {
-            path.add(at);
-        }
-        Collections.reverse(path);
+			for (City next : adj[curr.dest]) {
+				if (dist[next.dest] > curr.cost + next.cost) {
+					int newCost = curr.cost + next.cost;
+					dist[next.dest] = newCost;
+					pq.add(new City(newCost, next.dest));
+					p[next.dest] = curr.dest;
+				}
+			}
+		}
 
-        // Write output
-        bw.write(dist[end] + "\n");
-        bw.write(path.size() + "\n");
-        for (int city : path) {
-            bw.write(city + " ");
-        }
+		Deque<Integer> stack = new ArrayDeque<>();
+		int parent = end;
+		while (parent != 0) {
+			stack.push(parent);
+			parent = p[parent];
+		}
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		int minDist = dist[end];
+		int cnt = stack.size();
 
-    static class City {
-        int cost;
-        int dest;
+		bw.write(String.valueOf(minDist) + "\n");
+		bw.write(String.valueOf(cnt) + "\n");
+		while (!stack.isEmpty()) {
+			bw.write(String.valueOf(stack.pop() + " "));
+		}
 
-        City(int cost, int dest) {
-            this.cost = cost;
-            this.dest = dest;
-        }
-    }
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
+
+class City implements Comparable<City> {
+	int cost;
+	int dest;
+
+	City (int cost, int dest) {
+		this.cost = cost;
+		this.dest = dest;
+	}
+
+	@Override
+	public int compareTo(City o) {
+		return Integer.compare(this.cost, o.cost);
+	}
 }
