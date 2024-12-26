d46ff1eebbe13304fbcf1db8ea51de85daf3ff07
Tea._.pot <156047141+yngbao97@users.noreply.github.com>
2024-12-26 10:48:04 +0900

    [Silver I] Title: 스티커, Time: 884 ms, Memory: 111328 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,6 +1,5 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static BufferedReader br;
@@ -10,41 +9,45 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		br = new BufferedReader(new InputStreamReader(System.in));
+		//br = new BufferedReader(new InputStreamReader(new FileInputStream("src/"+Main.class.getPackage().getName()+"/input.txt")));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int t = Integer.parseInt(br.readLine());
 
-		for (int tc = 0; tc < t; tc++) {
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 0; tc < T; tc++) {
+			
 			int n = Integer.parseInt(br.readLine());
 			int[][] sticker = new int[2][n];
 			int[][] dp = new int[2][n];
-
+			
 			for (int i = 0; i < 2; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < n; j++) {
 					sticker[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-
+			
 			dp[0][0] = sticker[0][0];
 			dp[1][0] = sticker[1][0];
+			
 			for (int i = 1; i < n; i++) {
 				dp[0][i] = dp[1][i-1] + sticker[0][i];
 				dp[1][i] = dp[0][i-1] + sticker[1][i];
-
-				if (i >= 2) {
-					int max = Math.max(dp[0][i-2], dp[1][i-2]);
-					dp[0][i] = Math.max(dp[0][i], max + sticker[0][i]);
-					dp[1][i] = Math.max(dp[1][i], max + sticker[1][i]);
+				
+				if (i > 1) {
+					int front = Math.max(dp[0][i-2], dp[1][i-2]);
+					dp[0][i] = Math.max(dp[0][i], front + sticker[0][i]);
+					dp[1][i] = Math.max(dp[1][i], front + sticker[1][i]);
 				}
 			}
-
+			
 			int answer = Math.max(dp[0][n-1], dp[1][n-1]);
 			bw.write(String.valueOf(answer) + "\n");
 		}
-		
+
 		bw.flush();
 		bw.close();
 		br.close();
+
 	}
 }
